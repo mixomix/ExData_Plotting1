e8603a6ce69afb605cba87543f2817ba99d699a1
mixomix <mixomix@centrum.cz>
2017-02-24 18:44:34 +0100

    done

--- plot1.R
+++ plot1.R
@@ -5,9 +5,9 @@ dt <- read.csv2("c:/Users/mixomix/Desktop/household_power_consumption.txt",na.st
 dt$Date <- as.Date(dt$Date, format = '%d/%m/%Y')
 dt$Timestamp <- strptime(paste(as.character(dt$Date),dt$Time), format = "%Y-%m-%d %H:%M:%S")
 dt <- subset(dt, Date >= "2007-02-01" & Date <= "2007-02-02")
-dt$Global_active_power <- as.numeric(dt$Global_active_power)
+dt$Global_active_power <- as.numeric(as.character(dt$Global_active_power))
 
 png("plot1.png")
-hist(dt$Global_active_power/1000*2,main = "Global Active Power", col = "red" , xlab = "Global Active Power (kilowatts)")
+hist(dt$Global_active_power,main = "Global Active Power", col = "red" , xlab = "Global Active Power (kilowatts)")
 dev.off()
 
--- plot3.R
+++ plot3.R
@@ -1,21 +1,25 @@
 # read data & subset
-setwd("C:/Users/mixomix/Desktop/r/ExData_Plotting1/")
+setwd("C:/Users/mix/Desktop/ExData_Plotting1/")
 
-dt <- read.csv2("c:/Users/mixomix/Desktop/household_power_consumption.txt",na.strings = "?")
+dt <- read.csv2("c:/Users/mix/Desktop/household_power_consumption.txt",na.strings = "?")
 dt$Date <- as.Date(dt$Date, format = '%d/%m/%Y')
 dt$Timestamp <- strptime(paste(as.character(dt$Date),dt$Time), format = "%Y-%m-%d %H:%M:%S")
 dt <- subset(dt, Date >= "2007-02-01" & Date <= "2007-02-02")
-dt$Global_active_power <- as.numeric(dt$Global_active_power)
-dt$Sub_metering_1 <- as.numeric(dt$Sub_metering_1)
-dt$Sub_metering_2 <- as.numeric(dt$Sub_metering_2)
-dt$Sub_metering_3 <- as.numeric(dt$Sub_metering_3)
+
+options(digits=3)
+dt$Sub_metering_1 <- as.numeric(as.character(dt$Sub_metering_1))
+dt$Sub_metering_2 <- as.numeric(as.character(dt$Sub_metering_2))
+dt$Sub_metering_3 <- as.numeric(as.character(dt$Sub_metering_3))
+dt$Global_reactive_power <- as.numeric(as.character(dt$Global_reactive_power))
+dt$Voltage <- as.numeric(as.character(dt$Voltage))
 
 Sys.setlocale("LC_TIME", "English")
 
-png("plot2.png")
-par(ylog = TRUE)
-plot(dt$Timestamp,dt$Sub_metering_1,type = "l",main = "", ylab = "Energy sub metering", xlab = "")
+png("plot3.png")
 
-lines(dt$Timestamp,dt$Sub_metering_3/3,type = "l",main = "", col="red", ylab = "Energy sub metering", xlab = "")
+plot(dt$Timestamp,dt$Sub_metering_1,type = "l",main = "", ylab = "Energy sub metering", xlab = "")
+lines(dt$Timestamp,dt$Sub_metering_2,type = "l",main = "", col="red", ylab = "Energy sub metering", xlab = "")
+lines(dt$Timestamp,dt$Sub_metering_3,type = "l",main = "", col="blue", ylab = "Energy sub metering", xlab = "")
+legend("topright",lty = 1,col = c("black","red","blue") ,legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 dev.off()
 
